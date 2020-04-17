6fb002fbc0ff129110ebe81705ef07605fa4bdd4
DeployChef <dc.titovvlad@gmail.com>
2020-04-17 14:15:46 +0300

    Создал пустой проект для апи

---
+++ Backend/TodoService/TodoService/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TodoService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoController : Controller
+    {
+        private readonly ILogger<TodoController> _logger;
+
+        public TodoController(ILogger<TodoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public int Get()
+        {
+            return 1;
+        }
+    }
+}
